55de69ecb8854ebe1ae7c4027a06145f809cea4d
davidmedev222 <davidmedev@gmail.com>
2025-08-07 12:28:10 -0300

    build(lint): add biome configuration as linter

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 
 function HomePage() {
-  return (
-      <main>
-        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error fugit dolor consequatur ducimus necessitatibus perspiciatis id. Ab quisquam possimus voluptatum, assumenda ea ratione soluta in nemo! Soluta beatae eaque nesciunt.</h1>
-        <Button>Lorem</Button>
-      </main>
-  )
+	return (
+		<main>
+			<h1>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Error fugit
+				dolor consequatur ducimus necessitatibus perspiciatis id. Ab quisquam
+				possimus voluptatum, assumenda ea ratione soluta in nemo! Soluta beatae
+				eaque nesciunt.
+			</h1>
+			<Button>Lorem</Button>
+		</main>
+	);
 }
 
-export default HomePage
+export default HomePage;
