39b279bbd937c54df49c841957e812312c175bae
davidmedev222 <davidmedev@gmail.com>
2025-08-07 11:56:22 -0300

    chore: first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+function HomePage() {
+  return (
+      <main>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error fugit dolor consequatur ducimus necessitatibus perspiciatis id. Ab quisquam possimus voluptatum, assumenda ea ratione soluta in nemo! Soluta beatae eaque nesciunt.</h1>
+        <Button>Lorem</Button>
+      </main>
+  )
+}
+
+export default HomePage
